e5c2078ace2659f11368e262d98f35d7d6988c2e
Varulli <120419874+Varulli@users.noreply.github.com>
2024-12-03 13:45:25 -0500

    Move output to alternate buffer for readability

--- src/rotating-cube.c
+++ src/rotating-cube.c
@@ -1,5 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,22 +16,22 @@ typedef struct Quaternion
 Quaternion q_multiply(Quaternion q1, Quaternion q2)
 {
 	float R = q1.r * q2.r - q1.i * q2.i - q1.j * q2.j - q1.k * q2.k,
-		I = q1.r * q2.i + q1.i * q2.r + q1.j * q2.k - q1.k * q2.j,
-		J = q1.r * q2.j - q1.i * q2.k + q1.j * q2.r + q1.k * q2.i,
-		K = q1.r * q2.k + q1.i * q2.j - q1.j * q2.i + q1.k * q2.r;
+		  I = q1.r * q2.i + q1.i * q2.r + q1.j * q2.k - q1.k * q2.j,
+		  J = q1.r * q2.j - q1.i * q2.k + q1.j * q2.r + q1.k * q2.i,
+		  K = q1.r * q2.k + q1.i * q2.j - q1.j * q2.i + q1.k * q2.r;
 
-	Quaternion Q = { R, I, J, K };
+	Quaternion Q = {R, I, J, K};
 	return Q;
 }
 
 Quaternion q_rotate_about_x(Quaternion p, float angle)
 {
 	float half = angle * 0.5F,
-		cosa = cosf(half),
-		sina = sinf(half);
+		  cosa = cosf(half),
+		  sina = sinf(half);
 
-	Quaternion q1 = { cosa, sina, 0, 0 },
-		q2 = { cosa, -sina, 0, 0 };
+	Quaternion q1 = {cosa, sina, 0, 0},
+			   q2 = {cosa, -sina, 0, 0};
 
 	return q_multiply(q_multiply(q1, p), q2);
 }
@@ -41,11 +39,11 @@ Quaternion q_rotate_about_x(Quaternion p, float angle)
 Quaternion q_rotate_about_y(Quaternion p, float angle)
 {
 	float half = angle * 0.5F,
-		cosa = cosf(half),
-		sina = sinf(half);
+		  cosa = cosf(half),
+		  sina = sinf(half);
 
-	Quaternion q1 = { cosa, 0, sina, 0 },
-		q2 = { cosa, 0, -sina, 0 };
+	Quaternion q1 = {cosa, 0, sina, 0},
+			   q2 = {cosa, 0, -sina, 0};
 
 	return q_multiply(q_multiply(q1, p), q2);
 }
@@ -53,11 +51,11 @@ Quaternion q_rotate_about_y(Quaternion p, float angle)
 Quaternion q_rotate_about_z(Quaternion p, float angle)
 {
 	float half = angle * 0.5F,
-		cosa = cosf(half),
-		sina = sinf(half);
+		  cosa = cosf(half),
+		  sina = sinf(half);
 
-	Quaternion q1 = { cosa, 0, 0, sina },
-		q2 = { cosa, 0, 0, -sina };
+	Quaternion q1 = {cosa, 0, 0, sina},
+			   q2 = {cosa, 0, 0, -sina};
 
 	return q_multiply(q_multiply(q1, p), q2);
 }
@@ -66,7 +64,7 @@ Quaternion q_rotate_about_z(Quaternion p, float angle)
 #define WIDTH 100
 #define HEIGHT 50
 
-#define NUM_FRAMES 1000
+#define NUM_FRAMES 1500
 
 const int zDisp = 100;
 const int zProj = 50;
@@ -78,8 +76,18 @@ const float zLight = -1.0F;
 const float deltaTheta = 0.1F;
 const float deltaPhi = 0.05F;
 
+BOOL WINAPI HandlerRoutine(DWORD ctrlType)
+{
+	if (ctrlType == CTRL_C_EVENT)
+	{
+		printf("\x1b[?25h\x1b[?1049l");
+		fflush(stdout);
+		exit(0);
+	}
+	return FALSE;
+}
 
-void render_point(Quaternion q, Quaternion qNorm, float A, float B, float C, char** proj, float** zVal, float ooMagLight)
+void render_point(Quaternion q, Quaternion qNorm, float A, float B, float C, char **proj, float **zVal, float ooMagLight)
 {
 	q = q_rotate_about_z(q_rotate_about_y(q_rotate_about_x(q, A), B), C);
 	qNorm = q_rotate_about_z(q_rotate_about_y(q_rotate_about_x(qNorm, A), B), C);
@@ -108,35 +116,23 @@ void render_point(Quaternion q, Quaternion qNorm, float A, float B, float C, cha
 	}
 }
 
-void render_point2(Quaternion q, Quaternion qNorm, float A, float B, float C, char** proj, float** zVal, char fill)
-{
-	q = q_rotate_about_z(q_rotate_about_y(q_rotate_about_x(q, A), B), C);
-	qNorm = q_rotate_about_z(q_rotate_about_y(q_rotate_about_x(qNorm, A), B), C);
-
-	float ooz = 1 / (q.k + zDisp);
-
-	int xProj = (int)(WIDTH * 0.5F + q.i * zProj * ooz),
-		yProj = (int)(HEIGHT * 0.5F - q.j * zProj * ooz * 0.5F);
-
-	if (xProj >= 0 && xProj < WIDTH && yProj >= 0 && yProj < HEIGHT)
-	{
-		if (ooz > zVal[yProj][xProj])
-		{
-			zVal[yProj][xProj] = ooz;
-			proj[yProj][xProj] = fill;
-		}
-	}
-}
-
 int main()
 {
-	char** proj = calloc(HEIGHT, sizeof(char*));
-	float** zVal = calloc(HEIGHT, sizeof(float*));
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	DWORD mode;
+	if (GetConsoleMode(console, &mode))
+	{
+		SetConsoleMode(console, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+	}
+	SetConsoleCtrlHandler(HandlerRoutine, TRUE);
+
+	char **proj = calloc(HEIGHT, sizeof(char *));
+	float **zVal = calloc(HEIGHT, sizeof(float *));
 
 	if (proj == NULL || zVal == NULL)
 	{
-		printf("Failed memory allocation");
-		return -1;
+		printf("Failed memory allocation\n");
+		return 1;
 	}
 
 	for (int i = 0; i < HEIGHT; i++)
@@ -146,8 +142,8 @@ int main()
 
 		if (proj[i] == NULL || zVal[i] == NULL)
 		{
-			printf("Failed memory allocation");
-			return -1;
+			printf("Failed memory allocation\n");
+			return 1;
 		}
 	}
 
@@ -155,14 +151,15 @@ int main()
 
 	float ooMagLight = 1 / sqrtf(xLight * xLight + yLight * yLight + zLight * zLight);
 
-	getchar();
+	printf("\x1b[?1049h\x1b[?25l");
+	fflush(stdout);
 
 	for (int f = 0; f < NUM_FRAMES; f++)
 	{
-		printf("\x1b[H");
-
-		for (int i = 0; i < HEIGHT; i++) {
-			for (int j = 0; j < WIDTH; j++) {
+		for (int i = 0; i < HEIGHT; i++)
+		{
+			for (int j = 0; j < WIDTH; j++)
+			{
 				proj[i][j] = ' ';
 				zVal[i][j] = 0.0F;
 			}
@@ -171,8 +168,8 @@ int main()
 		for (int x = -25; x < 25; x++)
 			for (int y = -25; y < 25; y++)
 			{
-				Quaternion q = { 0, x, y, 25 },
-							qNorm = { 0, 0, 0, 1 };
+				Quaternion q = {0, x, y, 25},
+						   qNorm = {0, 0, 0, 1};
 
 				render_point(q, qNorm, A, B, C, proj, zVal, ooMagLight);
 			}
@@ -180,8 +177,8 @@ int main()
 		for (int x = -25; x < 25; x++)
 			for (int y = -25; y < 25; y++)
 			{
-				Quaternion q = { 0, x, y, -25 },
-					qNorm = { 0, 0, 0, -1 };
+				Quaternion q = {0, x, y, -25},
+						   qNorm = {0, 0, 0, -1};
 
 				render_point(q, qNorm, A, B, C, proj, zVal, ooMagLight);
 			}
@@ -189,8 +186,8 @@ int main()
 		for (int x = -25; x < 25; x++)
 			for (int z = -25; z < 25; z++)
 			{
-				Quaternion q = { 0, x, 25, z },
-					qNorm = { 0, 0, 1, 0 };
+				Quaternion q = {0, x, 25, z},
+						   qNorm = {0, 0, 1, 0};
 
 				render_point(q, qNorm, A, B, C, proj, zVal, ooMagLight);
 			}
@@ -198,8 +195,8 @@ int main()
 		for (int x = -25; x < 25; x++)
 			for (int z = -25; z < 25; z++)
 			{
-				Quaternion q = { 0, x, -25, z },
-					qNorm = { 0, 0, -1, 0 };
+				Quaternion q = {0, x, -25, z},
+						   qNorm = {0, 0, -1, 0};
 
 				render_point(q, qNorm, A, B, C, proj, zVal, ooMagLight);
 			}
@@ -207,8 +204,8 @@ int main()
 		for (int y = -25; y < 25; y++)
 			for (int z = -25; z < 25; z++)
 			{
-				Quaternion q = { 0, -25, y, z },
-					qNorm = { 0, -1, 0, 0 };
+				Quaternion q = {0, -25, y, z},
+						   qNorm = {0, -1, 0, 0};
 
 				render_point(q, qNorm, A, B, C, proj, zVal, ooMagLight);
 			}
@@ -216,24 +213,32 @@ int main()
 		for (int y = -25; y < 25; y++)
 			for (int z = -25; z < 25; z++)
 			{
-				Quaternion q = { 0, 25, y, z },
-					qNorm = { 0, 1, 0, 0 };
+				Quaternion q = {0, 25, y, z},
+						   qNorm = {0, 1, 0, 0};
 
 				render_point(q, qNorm, A, B, C, proj, zVal, ooMagLight);
 			}
 
-		char str[HEIGHT * (WIDTH + 1)] = { 0 };
-		for (int i = 0; i < HEIGHT; i++) {
-			for (int j = 0; j < WIDTH; j++) {
-				strncat(str, &proj[i][j], 1);
+		char str[HEIGHT * (WIDTH + 1) + 1] = {0};
+		for (int i = 0; i < HEIGHT; i++)
+		{
+			for (int j = 0; j < WIDTH; j++)
+			{
+				str[(WIDTH + 1) * i + j] = proj[i][j];
 			}
-			if (i - (HEIGHT - 1))
-				strcat(str, "\n");
+			str[(WIDTH + 1) * (i + 1) - 1] = '\n';
 		}
-		printf("%s", str);
+		printf("%s\x1b[H", str);
+		fflush(stdout);
 
-		A += 0.01F, B += 0.015F, C += 0.025F;
+		A += 0.006F, B += 0.008F, C += 0.01F;
 	}
+	printf("Press Enter to Return");
+	fflush(stdout);
+	getchar();
+
+	printf("\x1b[?25h\x1b[?1049l");
+	fflush(stdout);
 
 	return 0;
 }
